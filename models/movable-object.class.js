986cd0d77b70ce3da1f6bb06783a8b1ed3ba42b6
John Fieweger <john.fieweger98@gmail.com>
2022-05-10 21:00:39 +0200

    added hitbox

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -42,6 +42,22 @@ class MovableObject {
 
     }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        // Red rectangle
+        if (this instanceof Character || this instanceof Chicken) {
+            ctx.beginPath();
+            ctx.lineWidth = "6";
+            ctx.strokeStyle = "red";
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+
+    }
+
     moveRight() {
         this.x += this.speed;
     }
